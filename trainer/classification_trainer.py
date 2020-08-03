fc1a15d26416e8249a2420eb4f1537db8c97b9dd
Yexi Jiang <yxjiang@uber.com>
2020-08-02 21:36:55 -0700

    enable model save and load

--- trainer/classification_trainer.py
+++ trainer/classification_trainer.py
@@ -1,9 +1,14 @@
 # trainer
 import time
+import os
 import torch
 from torch.utils.tensorboard import SummaryWriter
 
-def train(model, config, train_dataloader, test_dataloader, check_interval=50, **kwargs):
+def train(
+        model, config, train_dataloader, test_dataloader, 
+        checkpoint_prefix, output_dir='/tmp/model', check_interval=50, **kwargs
+    ):
+    kwargs = kwargs['kwargs']
     criteria = config.criteria()
     optimizer = config.optimizer(model.parameters(), lr=config.lr)
     if 'weight_decay' in config.__dict__:
@@ -12,6 +17,15 @@ def train(model, config, train_dataloader, test_dataloader, check_interval=50, *
     counts = 0
     writer = SummaryWriter(filename_suffix=str(config))
     # writer.add_graph(model)
+    base_epoch = 0
+
+    if 'existing_checkpoint_filepath' in kwargs:
+        print('Load checkpoint from %s' % kwargs['existing_checkpoint_filepath'])
+        checkpoint = torch.load(kwargs['existing_checkpoint_filepath'])
+        model.load_state_dict(checkpoint['state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        base_epoch = checkpoint['epoch'] 
+
     torch.backends.cudnn.benchmark = True
     for epoch in range(config.epochs):
         for i, (word_ids, labels, x_lens, y_lens) in enumerate(train_dataloader):
@@ -22,7 +36,17 @@ def train(model, config, train_dataloader, test_dataloader, check_interval=50, *
             loss.backward()
             optimizer.step()
         if epoch % check_interval == 0 or epoch == config.epochs - 1:
-            print("[%d seconds](epoch: %d/%d)[%d samples] loss: %.3f." % (time.time() - start, epoch, config.epochs, counts, loss.mean().item()))
+            checkpoint_filename = checkpoint_prefix + '_' + str(epoch + base_epoch) + '.ckpt'
+            if not os.path.exists(output_dir):
+                os.mkdir(output_dir)
+            checkpoint_filepath = os.path.join(output_dir, checkpoint_filename)
+            torch.save({
+                'state_dict': model.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                'epoch': epoch + base_epoch,
+            }, checkpoint_filepath)
+            print('%d seconds(epoch: %d/%d), saved checkpoint file: %s' % (time.time() - start, epoch, config.epochs, checkpoint_filepath))
+            print('%d seconds](epoch: %d/%d)[%d samples] loss: %.3f.' % (time.time() - start, epoch, config.epochs, counts, loss.mean().item()))
             # eval on test dataset
             model.eval()
             with torch.no_grad():
