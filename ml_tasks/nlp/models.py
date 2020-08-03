02c205f083ab2dbbab88aeb63e291be040b4d92e
Yexi Jiang <yxjiang@uber.com>
2020-08-02 20:49:06 -0700

    move RNN to model.py

--- ml_tasks/nlp/models.py
+++ ml_tasks/nlp/models.py
@@ -1,9 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
-
-
+from torch.nn.utils.rnn import pack_padded_sequence
 
 
 class TextCNN(nn.Module):
@@ -35,4 +33,64 @@ class TextCNN(nn.Module):
         x = self.fc(x)
         x = x.view(batch, -1)
         
+        return x
+
+
+class RNN(nn.Module):
+    def __init__(self, config, vocabulary_size):
+        super().__init__()
+        self.config = config
+        self.rnn_type = config.rnn_type
+        self.embed = nn.Embedding(vocabulary_size, config.word_embedding_length)
+        self.num_layers = config.num_layers if config.num_layers is not None else 1
+        self.directions = config.num_directions if config.num_directions is not None else 1
+        self.hidden_size = 128
+        if config.rnn_type is nn.RNN:
+            self.rnn = nn.RNN(
+                input_size=config.word_embedding_length, 
+                hidden_size=self.hidden_size, 
+                num_layers=self.num_layers,
+                bidirectional=False if self.directions == 1 else True
+            )
+        elif config.rnn_type is nn.LSTM:
+            self.cell_size = 128
+            self.rnn = nn.LSTM(
+                input_size=config.word_embedding_length,
+                hidden_size=self.hidden_size,
+                num_layers=self.num_layers,
+                batch_first=True,
+                bidirectional=False if self.directions == 1 else True
+            )
+        elif config.rnn_type is nn.GRU:
+            self.rnn = nn.GRU(
+                input_size=config.word_embedding_length, 
+                hidden_size=self.hidden_size, 
+                num_layers=self.num_layers,
+                bidirectional=False if self.directions == 1 else True
+            )
+        self.fc1 = nn.Linear(in_features=self.hidden_size * self.directions, out_features=64)
+        self.dropout = nn.Dropout(config.dropout)
+        self.fc2 = nn.Linear(in_features=64, out_features=config.num_classes)
+
+    def forward(self, x, x_lens):
+        batch = x.shape[0]
+        x = self.embed(x)  # (batch, sentence_length, embedding_dim)
+        x = x.to(self.config.device)
+        x = pack_padded_sequence(x, x_lens, batch_first=True, enforce_sorted=False)
+
+        h0 = torch.zeros((self.num_layers * self.directions, batch, self.hidden_size)).to(self.config.device)
+        if self.rnn_type is nn.RNN or self.rnn_type is nn.GRU:
+            output, ht = self.rnn(x, h0)
+        elif self.rnn_type is nn.LSTM:
+            c0 = torch.zeros((self.num_layers * self.directions, batch, self.hidden_size)).to(self.config.device)
+            output, (ht, ct) = self.rnn(x, (h0, c0))
+        # decompose layers and directions, (layer, directions, batch, embedding_dim)
+        ht = ht.view(self.num_layers, self.directions, ht.shape[1], -1) 
+        ht = ht[-1]  # get last layer and remove layer dimension, (directions, batch, embedding_dim)
+        ht = ht.permute(1, 0, 2)  # (batch, directions, embedding_dim)
+        ht = ht.contiguous().view(batch, -1)
+        x = F.relu(self.fc1(ht))
+        x = self.dropout(x)
+        x = self.fc2(x)
+        x = x.view(batch, -1)
         return x
